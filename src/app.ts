e03cdd70272a433208d48b369e765e92ac5b0a0b
Neelaksh Mathur <neelakshm2@gmail.com>
2022-12-03 09:56:17 +0530

    Security

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { METHODS } from "http";
 
 const app = express();
@@ -54,7 +54,87 @@ app.use(express.json()); //Middleware
 //Route Handlers
 // get/post/put ->  METHODS
 // "/api/books" -> PATH
-// then -> Callback Function
+// then -> Callback Function -> Works the same as Middleware
+
+// function handleGetBookOne(req: Request, res: Response, next: NextFunction) {
+//   console.log(req.params);
+//   next();
+// }
+
+// function handleGetBookTwo(req: Request, res: Response, next: NextFunction) {
+//   console.log("Second Handler");
+//   return res.send(req.params);
+// }
+
+// app.get("/api/books/:bookId/:authorId", [handleGetBookOne, handleGetBookTwo]);
+//OR
+// app.get(
+//   "/api/books/:bookId/:authorId",
+//   function (req: Request, res: Response, next: NextFunction) {
+//     next();
+//   },
+//   function (req: Request, res: Response, next: NextFunction) {
+//     return res.send(req.params);
+//   }
+// );
+
+//Middleware
+// function middleware(req: Request, res: Response, next: NextFunction) {
+//   //@ts-ignore
+//   req.name = "Neelaksh";
+//   next();
+// }
+
+const middleware = //Currying
+
+    ({ name }: { name: string }) =>
+    (req: Request, res: Response, next: NextFunction) => {
+      //@ts-ignore
+      //req.name = name;
+      res.locals.name = name;
+      next();
+    };
+
+app.use(middleware({ name: "Neelaksh Mathur" })); //Global Middleware
+
+app.get(
+  "/api/books/:bookId/:authorId",
+
+  (req: Request, res: Response, next: NextFunction) => {
+    //(req: Request<{bookId:'string',authorId:string},{},{}>, res: Response, next: NextFunction)
+    //@ts-ignore
+    //console.log(req.name);
+    console.log(res.locals.name);
+    //@ts-ignore
+    //res.send(req.name);
+    res.send(res.locals.name);
+  }
+);
+
+//Error Handling
+// app.get("/error", () => {
+//   throw new Error("Boom!!");
+// });
+
+// async function throwsError() {
+//   throw new Error("Boom!!");
+// }
+
+// app.get("/error", async (req: Request, res: Response) => {
+//   try {
+//     await throwsError();
+//     res.sendStatus(200);
+//   } catch (e) {
+//     res.status(400).send("Something bad happened");
+//   }
+// });
+
+//Application Structure -> Controller Service Model
+
+//Security
+//yarn add helmet
+// import helmet from "helmet";
+// app.use(helmet());
 
 app.listen(3000, () => {
   console.log("Application listening at http://localhost:3000");
